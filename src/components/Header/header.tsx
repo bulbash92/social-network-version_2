2b734ff6febbf4df36afdf53822f3a42e9312aa0
Evgeniy <y.danchanka@tapston.com>
2022-09-09 11:27:35 +0300

    fix header layout

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,7 +1,25 @@
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../../hook';
 import styles from './header.module.css';
+import { User } from './userBlock/user';
 
 const Header = () => {
-  return <header className={styles.header}>MySocial</header>;
+  const {isAuth, login} = useAppSelector(state => state.auth)
+  const navigate = useNavigate()
+  const loginHandler = () => {
+    navigate('/login')
+  }
+
+  const logoutHandler = () => {
+    navigate('/login')
+  }
+
+  console.log(login)
+  return <header className={styles.header}>
+    <div>LOGO</div>
+    <h1>MySocial</h1>
+  <User isAuth={isAuth} login={login} logout={logoutHandler} onClickLogin={loginHandler}/>
+  </header>;
 };
 
 export default Header;
