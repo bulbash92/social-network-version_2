34285cd7d580cc215a1386d0b2c886ad940b7db2
Evgeniy <y.danchanka@tapston.com>
2022-09-09 16:49:46 +0300

    add redirect to login

--- src/components/pages/Profile/profile.tsx
+++ src/components/pages/Profile/profile.tsx
@@ -1,4 +1,10 @@
+import { Navigate } from "react-router-dom";
+import { useAppSelector } from "../../../hook";
+
 const Profile = () => {
+  const {isAuth} = useAppSelector(state => state.auth)
+  if (!isAuth) return <Navigate to={'/login'} />;
+
   return <div>Profile</div>;
 };
 export default Profile;
